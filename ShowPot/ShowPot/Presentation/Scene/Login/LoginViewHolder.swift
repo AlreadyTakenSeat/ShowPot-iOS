9ffdddfc4e404451f8c9ef0eac24c2cc4cc3a7a0
dlrjswns <leekyunjun@gmail.com>
2024-07-01 16:47:55 +0900

    #42 로그인화면에 필요한 UI 레이아웃 및 제약조건 추가 - 소셜로그인 kakao, google, apple 버튼 추가 - 백버튼, 로고, 알림라벨, 로그인이미지뷰 추가

--- ShowPot/ShowPot/Presentation/Scene/Login/LoginViewHolder.swift
+++ ShowPot/ShowPot/Presentation/Scene/Login/LoginViewHolder.swift
@@ -11,35 +11,102 @@ import GoogleSignIn
 
 final class LoginViewHolder {
     
-    let containerStackView = UIStackView().then {
-        $0.backgroundColor = .clear
-        $0.axis = .vertical
+    let backButton = UIButton().then {
+        $0.setImage(UIImage(resource: .icArrow36Left).withTintColor(.white), for: .normal)
     }
     
-    let googleSignInButton = GIDSignInButton()
+    let logoImageView = UIImageView().then {
+        $0.image = UIImage(resource: .showpot)
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    let alertLabel = UILabel().then {
+        $0.text = "잊지않고 내한공연 즐기러가요"
+        $0.textColor = .white
+        $0.font = KRFont.H2
+        $0.textAlignment = .center
+    }
+    
+    let loginImageView = UIImageView().then {
+        $0.image = UIImage(resource: .group1171275072)
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    let socialLoginStackView = UIStackView().then {
+        $0.backgroundColor = .clear
+        $0.axis = .vertical
+        $0.spacing = 12
+        $0.isLayoutMarginsRelativeArrangement = true
+        $0.layoutMargins = .init(top: .zero, left: 20, bottom: 56, right: 20)
+    }
+    
     let kakaoSignInButton = UIButton().then {
         $0.setImage(UIImage(resource: .kakao), for: .normal)
+        $0.layer.cornerRadius = 2
+        $0.layer.masksToBounds = true
+    }
+    
+    let googleSignInButton = UIButton().then {
+        $0.setImage(UIImage(resource: .google), for: .normal)
+        $0.layer.cornerRadius = 2
+        $0.layer.masksToBounds = true
+    }
+    
+    let appleSignInButton = UIButton().then {
+        $0.setImage(UIImage(resource: .apple), for: .normal)
+        $0.layer.cornerRadius = 2
+        $0.layer.masksToBounds = true
     }
 }
 
 extension LoginViewHolder: ViewHolderType {
     
     func place(in view: UIView) {
-        view.addSubview(containerStackView)
-        _ = [googleSignInButton, kakaoSignInButton].map { containerStackView.addArrangedSubview($0) }
+        _ = [backButton, logoImageView, alertLabel, loginImageView, socialLoginStackView].map { view.addSubview($0) }
+        _ = [kakaoSignInButton, googleSignInButton, appleSignInButton].map { socialLoginStackView.addArrangedSubview($0) }
     }
     
     func configureConstraints(for view: UIView) {
-        containerStackView.snp.makeConstraints {
-            $0.center.equalToSuperview()
+        
+        backButton.snp.makeConstraints {
+            $0.size.equalTo(36)
+            $0.leading.equalToSuperview().inset(6)
+            $0.top.equalToSuperview().inset(63)
         }
         
-        googleSignInButton.snp.makeConstraints {
-            $0.height.equalTo(50)
+        logoImageView.snp.makeConstraints {
+            $0.top.equalTo(backButton.snp.bottom).offset(29 + 8)
+            $0.leading.equalToSuperview().inset(126)
+            $0.trailing.equalToSuperview().inset(128.42)
+        }
+        
+        alertLabel.snp.makeConstraints {
+            $0.top.equalTo(logoImageView.snp.bottom).offset(8)
+            $0.directionalHorizontalEdges.equalToSuperview()
+            $0.height.equalTo(27)
+        }
+        
+        loginImageView.snp.makeConstraints {
+            $0.top.equalTo(alertLabel.snp.bottom).offset(42)
+            $0.leading.equalToSuperview().inset(114)
+            $0.trailing.equalToSuperview().inset(97.65)
+        }
+        
+        socialLoginStackView.snp.makeConstraints {
+            $0.top.greaterThanOrEqualTo(loginImageView.snp.bottom)
+            $0.directionalHorizontalEdges.bottom.equalToSuperview()
         }
         
         kakaoSignInButton.snp.makeConstraints {
-            $0.height.equalTo(50)
+            $0.height.equalTo(55)
+        }
+        
+        googleSignInButton.snp.makeConstraints {
+            $0.height.equalTo(55)
+        }
+        
+        appleSignInButton.snp.makeConstraints {
+            $0.height.equalTo(55)
         }
     }
     
