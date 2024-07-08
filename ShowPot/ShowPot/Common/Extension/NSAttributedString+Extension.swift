f68273f6e81152e7d167cdd0b61339558593be9c
dlrjswns <leekyunjun@gmail.com>
2024-07-08 19:36:30 +0900

    #37 NSMutableAttributedString -> NSAttributedString으로 변환
    - NSAttributedString이 NSMutableAttributedString의 상위버전이기에 확장성을 위해 변경

--- ShowPot/ShowPot/Common/Extension/NSAttributedString+Extension.swift
+++ ShowPot/ShowPot/Common/Extension/NSAttributedString+Extension.swift
@@ -7,12 +7,12 @@
 
 import UIKit
 
-extension NSMutableAttributedString {
+extension NSAttributedString {
     
     /// 줄 높이를 설정을 위한 attribute를 추가합니다.
     /// - Parameters:
     ///   - lineHeightMultiple: UILabel의 줄 높이 배수 (예: 1.5 = 150%)
-    func setLineHeight(lineHeightMultiple: CGFloat) -> Self {
+    func setLineHeight(lineHeightMultiple: CGFloat) -> NSAttributedString {
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineHeightMultiple = lineHeightMultiple
         
@@ -20,19 +20,21 @@ extension NSMutableAttributedString {
             .paragraphStyle: paragraphStyle
         ]
         
-        self.addAttributes(attributes, range: NSRange(location: 0, length: self.length))
-        return self
+        let mutableAttributedString = NSMutableAttributedString(attributedString: self)
+        mutableAttributedString.addAttributes(attributes, range: NSRange(location: 0, length: self.length))
+        return NSAttributedString(attributedString: mutableAttributedString)
     }
     
     /// 자간 설정을 위한 attribute를 추가합니다.
     /// - Parameters:
     ///   - letterSpacingPercent: UILabel의 자간 백분율 (예: -0.025 = -2.5%)
-    func setLetterSpacing(letterSpacingPercent: CGFloat) -> Self {
+    func setLetterSpacing(letterSpacingPercent: CGFloat) -> NSAttributedString {
         let attributes: [NSAttributedString.Key: Any] = [
             .kern: letterSpacingPercent
         ]
         
-        self.addAttributes(attributes, range: NSRange(location: 0, length: self.length))
-        return self
+        let mutableAttributedString = NSMutableAttributedString(attributedString: self)
+        mutableAttributedString.addAttributes(attributes, range: NSRange(location: 0, length: self.length))
+        return NSAttributedString(attributedString: mutableAttributedString)
     }
 }
