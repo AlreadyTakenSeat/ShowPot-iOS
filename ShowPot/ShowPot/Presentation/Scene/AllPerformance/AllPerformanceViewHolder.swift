90a50bf59cb35bf6f21ef2a087afde2ac833a615
dlrjswns <leekyunjun@gmail.com>
2024-08-02 18:50:57 +0900

    #96 전체공연화면 레이아웃 작성

--- ShowPot/ShowPot/Presentation/Scene/AllPerformance/AllPerformanceViewHolder.swift
+++ ShowPot/ShowPot/Presentation/Scene/AllPerformance/AllPerformanceViewHolder.swift
@@ -11,20 +11,42 @@ import SnapKit
 import Then
 
 final class AllPerformanceViewHolder {
-    let label = UILabel().then { label in
-        label.text = "Saved"
+    
+    let navigationView = AllPerformanceNavigationView()
+    
+    private let performanceListViewLayout = UICollectionViewFlowLayout().then {
+        $0.minimumLineSpacing = 10
+        $0.scrollDirection = .vertical
+        $0.sectionInset = .init(top: 8, left: 16, bottom: .zero, right: 16)
     }
+    
+    lazy var performanceListView = UICollectionView(frame: .zero, collectionViewLayout: performanceListViewLayout).then {
+        $0.register(FeaturedPerformanceWithTicketOnSaleSoonCell.self)
+        $0.register(PerformanceFilterHeaderView.self, of: UICollectionView.elementKindSectionHeader)
+        $0.backgroundColor = .gray700
+        $0.alwaysBounceVertical = true
+    }
+    
 }
 
 extension AllPerformanceViewHolder: ViewHolderType {
     
     func place(in view: UIView) {
-        view.addSubview(label)
+        
+        [navigationView, performanceListView].forEach { view.addSubview($0) }
     }
     
     func configureConstraints(for view: UIView) {
-        label.snp.makeConstraints { make in
-            make.center.equalToSuperview()
+        
+        navigationView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(12)
+            $0.directionalHorizontalEdges.equalToSuperview()
+            $0.height.equalTo(44)
+        }
+        
+        performanceListView.snp.makeConstraints {
+            $0.top.equalTo(navigationView.snp.bottom)
+            $0.directionalHorizontalEdges.bottom.equalToSuperview()
         }
     }
     
