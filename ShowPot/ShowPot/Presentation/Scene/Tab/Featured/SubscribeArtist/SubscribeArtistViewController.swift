cb70d5adb000ea34f8c5b65e9dde505926b4de48
dlrjswns <leekyunjun@gmail.com>
2024-08-06 00:06:40 +0900

    #105 아티스트구독화면 레이아웃 적용
    444레이아웃

--- ShowPot/ShowPot/Presentation/Scene/Tab/Featured/SubscribeArtist/SubscribeArtistViewController.swift
+++ ShowPot/ShowPot/Presentation/Scene/Tab/Featured/SubscribeArtist/SubscribeArtistViewController.swift
@@ -5,7 +5,7 @@
 //  Created by 이건준 on 8/5/24.
 //
 
-import Foundation
+import UIKit
 
 final class SubscribeArtistViewController: ViewController {
     let viewHolder: SubscribeArtistViewHolder = .init()
@@ -26,7 +26,32 @@ final class SubscribeArtistViewController: ViewController {
         viewHolderConfigure()
     }
     
+    override func setupStyles() {
+        super.setupStyles()
+        viewHolder.artistCollectionView.delegate = self
+    }
+    
     override func bind() {
+extension SubscribeArtistViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        .init(width: 100, height: 129)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        (collectionView.frame.width - 27 * 2 - 100 * 3) / 2
+    }
+}
+
+extension SubscribeArtistViewController {
+    func makeDataSource() -> SubscribeArtistViewModel.DataSource {
+        let cellRegistration = UICollectionView.CellRegistration<FeaturedSubscribeArtistCell, FeaturedSubscribeArtistCellModel> { (cell, indexPath, model) in
+            cell.configureUI(with: model)
+        }
         
+        let dataSource = UICollectionViewDiffableDataSource<SubscribeArtistViewModel.ArtistSection, FeaturedSubscribeArtistCellModel>(collectionView: viewHolder.artistCollectionView) { (collectionView, indexPath, model) -> UICollectionViewCell? in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: model)
+        }
+        return dataSource
     }
 }
--- ShowPot/ShowPot/Presentation/Scene/Tab/Featured/SubscribeArtist/SubscribeArtistViewHolder.swift
+++ ShowPot/ShowPot/Presentation/Scene/Tab/Featured/SubscribeArtist/SubscribeArtistViewHolder.swift
@@ -10,21 +10,75 @@ import SnapKit
 
 final class SubscribeArtistViewHolder {
     
-    let label = UILabel().then { label in
-        label.text = "Saved"
+    private var statusBarHeight: CGFloat {
+        let scenes = UIApplication.shared.connectedScenes
+        let windowScene = scenes.first as? UIWindowScene
+        
+        return windowScene?.statusBarManager?.statusBarFrame.height ?? 0
     }
+    
+    let topView = SubscribeArtistTopView()
+    
+    private let artistCollectionViewLayout = UICollectionViewFlowLayout().then {
+        $0.sectionInset = .init(top: 18, left: 27, bottom: .zero, right: 27)
+        $0.scrollDirection = .vertical
+        $0.minimumLineSpacing = 20
+    }
+    
+    lazy var artistCollectionView = UICollectionView(frame: .zero, collectionViewLayout: artistCollectionViewLayout).then {
+        $0.register(FeaturedSubscribeArtistCell.self)
+        $0.backgroundColor = .gray700
+        $0.alwaysBounceVertical = true
+    }
+    
+    lazy var subscribeButton = SubscribeButton(
+        frame: .init(
+            x: .zero,
+            y: UIScreen.main.bounds.height - statusBarHeight,
+            width: UIScreen.main.bounds.width,
+            height: 113
+        )
+    )
 }
 
 extension SubscribeArtistViewHolder: ViewHolderType {
     
     func place(in view: UIView) {
-        view.addSubview(label)
+        [topView, artistCollectionView, subscribeButton].forEach { view.addSubview($0) }
     }
     
     func configureConstraints(for view: UIView) {
-        label.snp.makeConstraints { make in
-            make.center.equalToSuperview()
+        topView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.directionalHorizontalEdges.equalToSuperview()
+            $0.height.equalTo(102)
+        }
+        
+        artistCollectionView.snp.makeConstraints {
+            $0.top.equalTo(topView.snp.bottom)
+            $0.directionalHorizontalEdges.bottom.equalToSuperview()
         }
     }
     
+}
+
+extension SubscribeArtistViewHolder {
+    
+    func showButton() {
+        UIView.animate(
+            withDuration: 0.1,
+            delay: 0.25,
+            options: .curveEaseIn) {
+                self.subscribeButton.frame.origin.y -= 113
+            }
+    }
+    
+    func dismissButton() {
+        UIView.animate(
+            withDuration: 0.1,
+            delay: 0.25,
+            options: .curveEaseOut) {
+                self.subscribeButton.frame.origin.y += 113
+            }
+    }
 }
