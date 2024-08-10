dafd6d96fe2be7f678db1b356142ffd679951897
dlrjswns <leekyunjun@gmail.com>
2024-08-11 01:56:39 +0900

    #107 기존 공연셀을 상속하여 코드 수정

--- ShowPot/ShowPot/Presentation/Scene/Tab/Saved/MyPerformanceAlarm/CustomView/PerformanceAlarmCell.swift
+++ ShowPot/ShowPot/Presentation/Scene/Tab/Saved/MyPerformanceAlarm/CustomView/PerformanceAlarmCell.swift
@@ -18,26 +18,9 @@ protocol PerformanceAlarmCellDelegate: AnyObject {
 }
 
 /// 공연알림정보에 대한 셀
-final class PerformanceAlarmCell: UICollectionViewCell, ReusableCell {
+final class PerformanceAlarmCell: PerformanceInfoCollectionViewCell {
     private var disposeBag = DisposeBag()
     weak var delegate: PerformanceAlarmCellDelegate?
-    
-    private let performanceImageView = UIImageView().then {
-        $0.contentMode = .scaleAspectFill
-        $0.layer.masksToBounds = true
-    }
-    
-    private let performanceTitleLabel = SPLabel(ENFont.H2).then {
-        $0.textColor = .gray000
-    }
-    
-    private let performanceTimeLabel = SPLabel(KRFont.B3_regular).then {
-        $0.textColor = .gray200
-    }
-    
-    private let performanceLocationLabel = SPLabel(KRFont.B3_regular).then {
-        $0.textColor = .gray200
-    }
     
     let alarmButton = PerformanceAlarmButtonView()
     
@@ -54,29 +37,14 @@ final class PerformanceAlarmCell: UICollectionViewCell, ReusableCell {
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        performanceImageView.image = nil
-        performanceTitleLabel.text = nil
-        performanceTimeLabel.text = nil
-        performanceLocationLabel.text = nil
         disposeBag = DisposeBag()
     }
     
     private func setupLayouts() {
-        [performanceImageView, performanceTitleLabel, performanceTimeLabel, performanceLocationLabel, alarmButton].forEach { contentView.addSubview($0) }
+        contentView.addSubview(alarmButton)
     }
     
     private func setupConstraints() {
-        performanceImageView.snp.makeConstraints {
-            $0.leading.directionalVerticalEdges.equalToSuperview()
-            $0.size.equalTo(80)
-        }
-        
-        performanceTitleLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(3)
-            $0.leading.equalTo(performanceImageView.snp.trailing).offset(14)
-            $0.trailing.lessThanOrEqualToSuperview()
-            $0.height.equalTo(33)
-        }
         
         alarmButton.snp.makeConstraints {
             $0.bottom.equalToSuperview().inset(3)
@@ -85,14 +53,14 @@ final class PerformanceAlarmCell: UICollectionViewCell, ReusableCell {
             $0.width.equalTo(58)
         }
         
-        performanceTimeLabel.snp.makeConstraints {
+        performanceTimeLabel.snp.remakeConstraints {
             $0.leading.equalTo(performanceTitleLabel)
             $0.trailing.lessThanOrEqualTo(alarmButton.snp.leading)
             $0.top.equalTo(performanceTitleLabel.snp.bottom).offset(5)
             $0.height.equalTo(18)
         }
         
-        performanceLocationLabel.snp.makeConstraints {
+        performanceLocationLabel.snp.remakeConstraints {
             $0.leading.equalTo(performanceTimeLabel)
             $0.trailing.lessThanOrEqualTo(alarmButton.snp.leading)
             $0.top.equalTo(performanceTimeLabel.snp.bottom).offset(1)
@@ -107,36 +75,5 @@ final class PerformanceAlarmCell: UICollectionViewCell, ReusableCell {
                 owner.delegate?.didTappedAlarmButton(owner)
             }
             .disposed(by: disposeBag)
-    }
-}
-
-extension PerformanceAlarmCell {
-    func configureUI(with model: PerformanceInfoCollectionViewCellModel) {
-        self.configureUI(
-            performanceImageURL: model.performanceImageURL,
-            performanceTitle: model.performanceTitle,
-            performanceTime: model.performanceTime,
-            performanceLocation: model.performanceLocation
-        )
-    }
-    
-    func configureUI(
-        performanceImageURL: URL?,
-        performanceTitle: String,
-        performanceTime: Date?,
-        performanceLocation: String
-    ) {
-        let formattedTime: String
-        if let time = performanceTime {
-            formattedTime = DateFormatterFactory.dateWithDot.string(from: time)
-        } else {
-            LogHelper.error("서버에서 내려준 포맷과 일치하지않습니다, 확인해주세요.")
-            formattedTime = ""
-        }
-        
-        performanceImageView.kf.setImage(with: performanceImageURL)
-        performanceTitleLabel.setText(performanceTitle)
-        performanceTimeLabel.setText(formattedTime)
-        performanceLocationLabel.setText(performanceLocation)
     }
 }
