24a8c3a0188b9b9b1500b99dc38264a3fad1ed00
dlrjswns <leekyunjun@gmail.com>
2024-08-16 01:12:12 +0900

    #124 디자인 수정으로 인한 내공연메인화면 로직 수정
    - 변경된 SwiftGen코드 수정
    - 메뉴 탭 데이터 변경

--- ShowPot/ShowPot/Presentation/Scene/Tab/Saved/SavedCoordinator.swift
+++ ShowPot/ShowPot/Presentation/Scene/Tab/Saved/SavedCoordinator.swift
@@ -28,16 +28,12 @@ final class SavedCoordinator: NavigationCoordinator {
         self.childCoordinators.append(coordinator)
     }
     
-    func goToSubscribeArtistScreen() {
-        let coordinator = SubscribeArtistCoordinator(navigationController: self.navigationController)
-        coordinator.start()
-        self.childCoordinators.append(coordinator)
+    func goToEndShowScreen() {
+        LogHelper.debug("티켓팅 종료 공연 화면으로 이동")
     }
     
-    func goToSubscribeGenreScreen() {
-        let coordinator = SubscribeGenreCoordinator(navigationController: self.navigationController)
-        coordinator.start()
-        self.childCoordinators.append(coordinator)
+    func goToInterestShowScreen() {
+        LogHelper.debug("관심 공연 화면으로 이동")
     }
     
     func goToLoginScreen() {
