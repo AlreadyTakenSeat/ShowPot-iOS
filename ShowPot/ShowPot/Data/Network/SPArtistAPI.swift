3c0e9b7c0ec6315dc6abb5f5fb64f619551ed8c4
dlrjswns <leekyunjun@gmail.com>
2024-08-29 03:17:14 +0900

    #164 검색화면에서 사용한 API 및 TargetType 생성

--- ShowPot/ShowPot/Data/Network/SPArtistAPI.swift
+++ ShowPot/ShowPot/Data/Network/SPArtistAPI.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import Alamofire
+
+enum SPArtistTargetType: APIType {
+    
+    /// 아티스트 구독
+    case subscribe
+    
+    /// 아티스독 구독 해제
+    case unsubscribe
+    
+    /// 구독한 아티스트 리스트
+    case subscriptions
+    
+    /// 구독하지않은 아티스트 리스트
+    case unsubscriptions
+    
+    var baseURL: String {
+        return Environment.baseURL
+    }
+    
+    var method: Alamofire.HTTPMethod {
+        switch self {
+        case .subscriptions, .unsubscriptions:
+            return .get
+        case .subscribe, .unsubscribe:
+            return .post
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .subscribe:
+            return "artists/subscribe"
+        case .unsubscribe:
+            return "artists/unsubscribe"
+        case .subscriptions:
+            return "artists/subscriptions"
+        case .unsubscriptions:
+            return "artists/unsubscriptions"
+        }
+    }
+}
+
