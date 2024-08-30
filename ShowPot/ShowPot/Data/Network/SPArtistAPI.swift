d03980e1754510558ead7c0b826739fc6e1736e8
dlrjswns <leekyunjun@gmail.com>
2024-08-30 13:37:59 +0900

    #171 아티스트관련 API 구현

--- ShowPot/ShowPot/Data/Network/SPArtistAPI.swift
+++ ShowPot/ShowPot/Data/Network/SPArtistAPI.swift
@@ -5,7 +5,7 @@
 //  Created by 이건준 on 8/29/24.
 //
 
-import Foundation
+import RxSwift
 import Alamofire
 
 enum SPArtistTargetType: APIType {
@@ -45,6 +45,84 @@ enum SPArtistTargetType: APIType {
             return "artists/subscriptions"
         case .unsubscriptions:
             return "artists/unsubscriptions"
+        }
+    }
+}
+
+final class SPArtistAPI {
+    
+    func unsubscriptions(request: UnsubscribeArtistListRequest) -> Observable<ArtistListResponse> {
+        
+        let target = SPTargetType.unsubscribeArtistList
+        
+        LogHelper.info("[\(target.method)] \(target.url)")
+        LogHelper.info("구독안한아티스트리스트 요청정보: \(request)")
+        
+        return Observable.create { emitter in
+            AF.request(
+                target.url,
+                method: target.method,
+                parameters: request
+            ).responseDecodable(of: ArtistListResponse.self) { response in
+                switch response.result {
+                case .success(let data):
+                    emitter.onNext(data)
+                    emitter.onCompleted()
+                case .failure(let error):
+                    LogHelper.error("\(error.localizedDescription): \(error)")
+                    emitter.onError(error)
+                }
+            }
+            
+            return Disposables.create()
+        }
+    }
+    
+    func unsubscribe(artistIDS: [String]) -> Observable<UnsubscribeArtistResponse> {
+        let target = SPArtistTargetType.unsubscribe
+        let request = UnsubscribeArtistRequest(artistIDS: artistIDS)
+        return Observable.create { emitter in
+            AF.request(
+                target.url,
+                method: target.method,
+                parameters: request,
+                encoder: JSONParameterEncoder.default,
+                headers: target.header
+            ).responseDecodable(of: UnsubscribeArtistResponse.self) { response in
+                switch response.result {
+                case .success(let data):
+                    emitter.onNext(data)
+                    emitter.onCompleted()
+                case .failure(let error):
+                    LogHelper.error("\(error.localizedDescription): \(error)")
+                    emitter.onError(error)
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    func subscribe(artistIDS: [String]) -> Observable<SubscribeArtistResponse> {
+        let target = SPArtistTargetType.subscribe
+        let request = SubscribeArtistRequest(artistIDS: artistIDS)
+        return Observable.create { emitter in
+            AF.request(
+                target.url,
+                method: target.method,
+                parameters: request,
+                encoder: JSONParameterEncoder.default,
+                headers: target.header
+            ).responseDecodable(of: SubscribeArtistResponse.self) { response in
+                switch response.result {
+                case .success(let data):
+                    emitter.onNext(data)
+                    emitter.onCompleted()
+                case .failure(let error):
+                    LogHelper.error("\(error.localizedDescription): \(error)")
+                    emitter.onError(error)
+                }
+            }
+            return Disposables.create()
         }
     }
 }
