fcf789bb5b1ad7b67317ad76c5607ea8320f1e23
Daegun Choi  <chcs1370@gmail.com>
2024-08-30 15:38:16 +0900

    #177 알림 설정한 공연 API 연결

--- ShowPot/ShowPot/Domain/UseCase/MyShow/MyShowAlarmUseCase.swift
+++ ShowPot/ShowPot/Domain/UseCase/MyShow/MyShowAlarmUseCase.swift
@@ -6,50 +6,85 @@
 //
 import Foundation
 import RxSwift
-import RxCocoa
+import RxRelay
 
 final class MyShowAlarmUseCase: MyShowUseCase {
-    var updateTicketingAlarmResult: RxRelay.PublishRelay<Bool> = PublishRelay<Bool>()
     
-    var ticketingAlarm: RxRelay.PublishRelay<[TicketingAlarmCellModel]> = PublishRelay<[TicketingAlarmCellModel]>()
-    var showList: RxRelay.BehaviorRelay<[PerformanceInfoCollectionViewCellModel]> = BehaviorRelay<[PerformanceInfoCollectionViewCellModel]>(value: [])
+    private let showAPI = SPShowAPI()
+    private let disposeBag = DisposeBag()
     
-    func fetchShowList() { // FIXME: - #6 내 공연정보 API 요청
-        showList.accept([
-            .init(showID: "1", performanceImageURL: URL(string: "https://img.newspim.com/news/2023/09/21/2309210928580280.jpg"), performanceTitle: "에스파단독고연", performanceTime: Date(timeIntervalSinceNow: 24 * 60 * 60), performanceLocation: "KBS 아레나홀"),
-            .init(showID: "2", performanceImageURL: URL(string: "https://image.bugsm.co.kr/essential/images/500/39/3978.jpg"), performanceTitle: "샘스미스단독공연", performanceTime: Date(timeIntervalSinceNow: 24 * 60), performanceLocation: "KBS 아레나홀"),
-            .init(showID: "3", performanceImageURL: URL(string: "https://img.newspim.com/news/2023/03/06/2303060940437270.jpg"), performanceTitle: "안젤리나졸리단독고연", performanceTime: Date(timeIntervalSinceNow: 24 * 60 * 60 * 60), performanceLocation: "KBS 아레나홀"),
-            .init(showID: "4", performanceImageURL: URL(string: "https://www.gugakpeople.com/data/gugakpeople_com/mainimages/202407/2024071636209869.jpg"), performanceTitle: "브루노마스단독고연", performanceTime: Date(timeIntervalSinceNow: 24), performanceLocation: "KBS 아레나홀"),
-            .init(showID: "5", performanceImageURL: URL(string: "https://img.newspim.com/news/2023/06/05/2306051642145540.jpg"), performanceTitle: "워시단독고연", performanceTime: Date(timeIntervalSinceNow: 24 * 60 * 60), performanceLocation: "KBS 아레나홀")
-        ])
+    var updateTicketingAlarmResult = PublishRelay<Bool>()
+    var ticketingAlarm = PublishRelay<[TicketingAlarmCellModel]>()
+    var showList = BehaviorRelay<[PerformanceInfoCollectionViewCellModel]>(value: [])
+    
+    func fetchShowList() {
+        
+        showAPI.alertList()
+            .map { response in
+                response.data.map { showData in
+                    PerformanceInfoCollectionViewCellModel(
+                        showID: showData.id,
+                        performanceImageURL: URL(string: showData.imageURL),
+                        performanceTitle: showData.title,
+                        performanceTime: showData.startAt.date("yyyy.MM.dd"),
+                        performanceLocation: showData.location
+                    )
+                }
+            }.bind(to: showList)
+            .disposed(by: disposeBag)
     }
     
-    func updateTicketingAlarm(model: [TicketingAlarmCellModel], showID: String) { // FIXME: - #6 내 공연 티켓팅정보 업데이트 API 요청
+    func updateTicketingAlarm(model: [TicketingAlarmCellModel], showID: String) {
         LogHelper.debug("티켓팅 알림 업데이트 정보: \(model.filter { $0.isEnabled && $0.isChecked })\n업데이트할 공연아이디: \(showID)")
-        let result = [true, false].shuffled()[0]
-        LogHelper.debug("티켓팅 알림 업데이트 결과: \(result)")
-        updateTicketingAlarmResult.accept(result)
+        showAPI.updateAlert(showId: showID)
+            .subscribe {
+                self.updateTicketingAlarmResult.accept(true)
+            } onError: { error in
+                self.updateTicketingAlarmResult.accept(false)
+            }
+            .disposed(by: disposeBag)
     }
     
     func deleteShowAlarm(indexPath: IndexPath) {
-        var currentShowList = showList.value
-        LogHelper.debug("알림 해제한 공연정보: \(currentShowList[indexPath.row])")
-        let deleteShowID = currentShowList[indexPath.row].showID
-        currentShowList.removeAll(where: { $0.showID == deleteShowID })
-        showList.accept(currentShowList)
+        LogHelper.debug("알림 해제한 공연정보: \(showList.value[indexPath.row])")
+        let deleteShowID = showList.value[indexPath.row].showID
+        showAPI.updateAlert(showId: deleteShowID)
+            .subscribe {
+                self.updateTicketingAlarmResult.accept(true)
+            } onError: { error in
+                self.updateTicketingAlarmResult.accept(false)
+            } onCompleted: {
+                self.fetchShowList()
+            }
+            .disposed(by: disposeBag)
     }
     
-    func requestTicketingAlarm() { // FIXME: - #6 내 공연 티켓팅정보 API 요청
-        
-        let currentDate = Date()
-        let calendar = Calendar.current
-        
-        let reserveDate = calendar.date(byAdding: .hour, value: 24, to: currentDate) ?? Date() // TODO: - 추후 기준이 될 일반예매시간으로 대체
-        
-        ticketingAlarm.accept([
-            .init(isChecked: true, isEnabled: reserveDate.hasElapsed(hours: 24), ticketingAlertText: "티켓팅 24시간 전"),
-            .init(isChecked: true, isEnabled: reserveDate.hasElapsed(hours: 6), ticketingAlertText: "티켓팅 6시간 전"),
-            .init(isChecked: false, isEnabled: reserveDate.hasElapsed(hours: 1), ticketingAlertText: "티켓팅 1시간 전")
-        ])
+    // TODO: #178 show id 인자로 받도록 수정
+    func requestTicketingAlarm(/*showId: String*/) {
+        let showId = ""
+        showAPI.reservationInfo(showId: showId)
+            .map { response in
+                let availability = response.alertReservationAvailability
+                let status = response.alertReservationStatus
+                return [
+                    TicketingAlarmCellModel(
+                        isChecked: availability.canReserve24,
+                        isEnabled: status.before24,
+                        ticketingAlertText: "티켓팅 24시간 전"
+                    ),
+                    TicketingAlarmCellModel(
+                        isChecked: availability.canReserve24,
+                        isEnabled: status.before24,
+                        ticketingAlertText: "티켓팅 24시간 전"
+                    ),
+                    TicketingAlarmCellModel(
+                        isChecked: availability.canReserve24,
+                        isEnabled: status.before24,
+                        ticketingAlertText: "티켓팅 24시간 전"
+                    ),
+                ]
+            }
+            .bind(to: ticketingAlarm)
+            .disposed(by: disposeBag)
     }
 }
