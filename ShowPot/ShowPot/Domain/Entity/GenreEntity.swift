dd856e1b99eca0135fd3b398b994937a567f4422
김도형 <shapekim98@gmail.com>
2025-03-31 21:13:18 +0900

    [design] #4 장르 셀 구성

---
+++ ShowPot/ShowPot/Domain/Entity/GenreEntity.swift
@@ -0,0 +1,32 @@
+//
+//  GenreEntity.swift
+//  ShowPot
+//
+//  Created by 김도형 on 3/31/25.
+//
+
+import Foundation
+
+struct GenreEntity: Identifiable {
+    let id: String
+    let name: Genre?
+    let isSubscribed: Bool?
+}
+
+extension GenreEntity {
+    enum Genre: String {
+        case rock
+        case hiphop
+        case band
+        case jpop
+        case pop
+        case house
+        case edm
+        case musical
+        case rnb
+        case opera
+        case metal
+        case classic
+        case jazz
+    }
+}
---
+++ ShowPot/ShowPot/Presentation/DesignSystem/Cell/GenreCell.swift
@@ -0,0 +1,114 @@
+//
+//  GenreCell.swift
+//  ShowPot
+//
+//  Created by 김도형 on 3/31/25.
+//
+
+import Foundation
+
+import UIKit
+
+import Kingfisher
+import SnapKit
+
+final class GenreCell: UICollectionViewCell {
+    private let imageView = UIImageView()
+    private let deleteButton = UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+        
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func registration(genre: GenreEntity, isDelete: Bool) {
+        if let isSubscribed = genre.isSubscribed {
+            imageView.image = isSubscribed
+            ? genre.name?.subscribedImage
+            : genre.name?.image
+        } else {
+            configurationUpdateHandler = { [weak self] cell, state in
+                self?.imageView.image = state.isSelected
+                ? genre.name?.selectedImage
+                : genre.name?.image
+            }
+        }
+        
+        deleteButton.isHidden = !isDelete
+    }
+}
+
+// MARK: - Configure Views
+private extension GenreCell {
+    func configureUI() {
+        contentView.backgroundColor = .clear
+        
+        configureImageView()
+        
+        configureDeleteButton()
+    }
+    
+    func configureLayout() {
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.size.equalTo(100)
+        }
+        
+        deleteButton.snp.makeConstraints { make in
+            make.top.trailing.equalToSuperview()
+        }
+    }
+    
+    func configureImageView() {
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        contentView.addSubview(imageView)
+    }
+    
+    func configureDeleteButton() {
+        var configuration = UIButton.Configuration.plain()
+        configuration.image = .icXmarkCircle.resized(
+            to: CGSize(
+                width: 30,
+                height: 30
+            )
+        )
+        configuration.contentInsets = .zero
+        deleteButton.configuration = configuration
+        deleteButton.isHidden = true
+        contentView.addSubview(deleteButton)
+    }
+}
+
+extension GenreEntity.Genre {
+    var image: UIImage? {
+        return UIImage(named: "genre_\(self.rawValue)")
+    }
+    
+    var selectedImage: UIImage? {
+        return UIImage(named: "genre_selected_\(self.rawValue)")
+    }
+    
+    var subscribedImage: UIImage? {
+        return UIImage(named: "genre_subscribed_\(self.rawValue)")
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview {
+    let cell = GenreCell()
+    cell.registration(genre: GenreResponse.mock.toEntity(), isDelete: false)
+    cell.isSelected = true
+    cell.snp.makeConstraints { make in
+        make.width.equalTo(140)
+        make.height.equalTo(140)
+    }
+    return cell
+}
