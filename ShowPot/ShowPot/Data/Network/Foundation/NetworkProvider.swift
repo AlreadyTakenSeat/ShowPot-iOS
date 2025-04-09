bdac9d20236dd848f8b27e8fb5dba3f82c21599c
김도형 <shapekim98@gmail.com>
2025-04-09 16:15:12 +0900

    [feat] #16 네트워크 코드 추가

---
+++ ShowPot/ShowPot/Data/Network/Foundation/NetworkProvider.swift
@@ -0,0 +1,120 @@
+//
+//  NetworkProvider.swift
+//  CoinCo
+//
+//  Created by 김도형 on 3/7/25.
+//
+
+import Foundation
+
+import Alamofire
+
+struct NetworkProvider<E: EndPoint>: Sendable {
+    func request<T: Decodable>(_ endPoint: E) async throws -> T {
+#if DEBUG
+        try NetworkLogger.request(endPoint)
+#endif
+        let response = await AF.request(
+            endPoint,
+            interceptor: TokenInterceptor()
+        )
+        .validate(statusCode: 200..<300)
+        .serializingDecodable(T.self, decoder: endPoint.decoder)
+        .response
+#if DEBUG
+        try NetworkLogger.response(response)
+#endif
+        switch response.result {
+        case .success(let value):
+            return value
+        case .failure(let error):
+            guard let data = response.data else {
+                throw error
+            }
+            //            throw endPoint.error(
+            //                response.response?.statusCode,
+            //                data: data
+            //            )
+            throw error
+        }
+    }
+    
+    func request(_ endPoint: E) async throws {
+#if DEBUG
+        try NetworkLogger.request(endPoint)
+#endif
+        let response = await AF.request(
+            endPoint,
+            interceptor: TokenInterceptor()
+        )
+        .validate(statusCode: 200..<300)
+        .serializingData()
+        .response
+#if DEBUG
+        try NetworkLogger.response(response)
+#endif
+        switch response.result {
+        case .success: return
+        case .failure(let error):
+            guard let data = response.data else {
+                throw error
+            }
+            //            throw endPoint.error(
+            //                response.response?.statusCode,
+            //                data: data
+            //            )
+            throw error
+        }
+    }
+    
+    func requestNonToken<T: Decodable>(_ endPoint: E) async throws -> T {
+#if DEBUG
+        try NetworkLogger.request(endPoint)
+#endif
+        let response = await AF.request(endPoint)
+            .validate(statusCode: 200..<300)
+            .serializingDecodable(T.self, decoder: endPoint.decoder)
+            .response
+#if DEBUG
+        try NetworkLogger.response(response)
+#endif
+        switch response.result {
+        case .success(let value):
+            return value
+        case .failure(let error):
+            guard let data = response.data else {
+                throw error
+            }
+            //            throw endPoint.error(
+            //                response.response?.statusCode,
+            //                data: data
+            //            )
+            throw error
+        }
+    }
+    
+    func requestNonToken(_ endPoint: E) async throws {
+#if DEBUG
+        try NetworkLogger.request(endPoint)
+#endif
+        let response = await AF.request(endPoint)
+            .validate(statusCode: 200..<300)
+            .serializingData()
+            .response
+#if DEBUG
+        try NetworkLogger.response(response)
+#endif
+        switch response.result {
+        case .success: return
+        case .failure(let error):
+            guard let data = response.data else {
+                throw error
+            }
+            //            throw endPoint.error(
+            //                response.response?.statusCode,
+            //                data: data
+            //            )
+            throw error
+        }
+    }
+}
---
+++ ShowPot/ShowPot/Data/Network/Foundation/TokenInterceptor.swift
@@ -0,0 +1,68 @@
+//
+//  TokenInterceptor.swift
+//  ShowPot
+//
+//  Created by 김도형 on 4/8/25.
+//
+
+import Foundation
+
+import Alamofire
+import Dependencies
+
+final class TokenInterceptor: RequestInterceptor {
+    func adapt(
+        _ urlRequest: URLRequest,
+        for session: Session,
+        completion: @escaping (Result<URLRequest, Error>) -> Void
+    ) {
+        @Dependency(\.keychainProvider.read)
+        var keychainRead
+        
+        var request = urlRequest
+        
+        guard let accessToken = keychainRead(.accessToken) else {
+            completion(.failure(SPError.tokenNotFound))
+            return
+        }
+        request.setValue(
+            "Bearer \(accessToken)",
+            forHTTPHeaderField: "Authorization"
+        )
+        completion(.success(request))
+    }
+    
+    func retry(
+        _ request: Request,
+        for session: Session,
+        dueTo error: any Error,
+        completion: @escaping (RetryResult) -> Void
+    ) {
+        guard let response = request.task?.response as? HTTPURLResponse else {
+            completion(.doNotRetryWithError(error))
+            return
+        }
+        guard response.statusCode == 401 else {
+            completion(.doNotRetryWithError(error))
+            return
+        }
+        @Dependency(\.keychainProvider.read)
+        var keychainRead
+        @Dependency(\.keychainProvider.save)
+        var keyChainSave
+        let provider = NetworkProvider<UsersEndPoint>()
+        
+        guard let refreshToken = keychainRead(.refreshToken) else {
+            completion(.doNotRetryWithError(SPError.tokenNotFound))
+            return
+        }
+        Task {
+            let response: TokenResponse = try await provider.requestNonToken(
+                .reissue(refreshToken)
+            )
+            keyChainSave(response.accessToken, .accessToken)
+            keyChainSave(response.refreshToken, .refreshToken)
+            completion(.retry)
+        }
+    }
+}
